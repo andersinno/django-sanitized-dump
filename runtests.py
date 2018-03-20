9f1418378a3b146a54ef4a3dfe643305dc60fd84
Frank Wickström <frank.wickstrom@anders.fi>
2018-03-20 15:57:18 +0200

    Add test setup

---
+++ runtests.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+import sys
+import django
+import pytest
+import py
+from django.conf import settings, global_settings as default_settings
+from django.core.management import execute_from_command_line
+from os import path
+
+# Give feedback on used versions
+sys.stderr.write('Using Python version {0} from {1}\n'.format(sys.version[:5], sys.executable))
+sys.stderr.write('Using Django version {0} from {1}\n'.format(
+    django.get_version(),
+    path.dirname(path.abspath(django.__file__)))
+)
+
+if not settings.configured:
+    module_root = path.dirname(path.realpath(__file__))
+    sys.path.insert(0, path.join(module_root, 'tests'))
+
+
+    template_settings = dict(
+        TEMPLATES = [
+            {
+                'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                'DIRS': [],
+                'APP_DIRS': True,
+                'OPTIONS': {
+                    'context_processors': [
+                        'django.template.context_processors.debug',
+                        'django.template.context_processors.request',
+                        'django.contrib.auth.context_processors.auth',
+                        'django.contrib.messages.context_processors.messages',
+                    ],
+                },
+            },
+        ]
+    )
+
+    MIDDLEWARE = [
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+    ]
+
+    settings.configure(
+        DEBUG = False,
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': ':memory:'
+            }
+        },
+        INSTALLED_APPS = (
+            'django.contrib.auth',
+            'django.contrib.contenttypes',
+            'django.contrib.admin',
+            'django.contrib.sessions',
+            'sanitized_dump',
+            'testapp'
+        ),
+        MIDDLEWARE_CLASSES=MIDDLEWARE,
+        MIDDLEWARE=MIDDLEWARE,
+        ROOT_URLCONF = [],
+        **template_settings
+    )
+
+DEFAULT_TEST_APPS = [
+    'tests',
+]
+
+def runtests():
+    other_args = list(filter(lambda arg: arg.startswith('-'), sys.argv[1:]))
+    test_apps = list(filter(lambda arg: not arg.startswith('-'), sys.argv[1:])) or DEFAULT_TEST_APPS
+
+    argv = sys.argv[:1] + other_args + test_apps
+    py.test.cmdline.main(argv)
+
+if __name__ == '__main__':
+    runtests()
