4728712c0317e48c904cc532ed8334b6c5830e1f
Frank Wickström <frank.wickstrom@anders.fi>
2018-03-21 09:09:10 +0200

    Remove print statements

--- tests/test_model_utils.py
+++ tests/test_model_utils.py
@@ -7,10 +7,8 @@ from sanitized_dump.utils import models
 
 class TestModelUtils():
     def test_get_model_table_name(self):
-        print(models.get_model_table_name(Name))
         assert models.get_model_table_name(Name) == 'testapp_name'
         assert True
 
     def test_get_model_field_names(self):
-        print(models.get_model_field_names(Name))
         assert models.get_model_field_names(Name) == ['id', 'name']
