f95774bb941630fa9246fb46361133b8517c22c7
Frank Wickström <frank.wickstrom@anders.fi>
2018-03-21 15:06:07 +0200

    Add configuration tests
    Co-authored-by: Jesse Laukkanen <jesse.laukkanen@anders.fi>

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,16 +1,52 @@
 # -*- coding: utf-8 -*-
-from django.test import TestCase
+import yaml
+from mock import patch
+from mock_open import MockOpen
 
 from sanitized_dump.utils import models
+from sanitized_dump.utils.compat import builtins_open
 from sanitized_dump.config import Configuration
 
-from testapp.models import Secret
 
-class TestModelUtils:
+def assert_all_models_in_conf(config):
+    model_table_names = models.get_model_table_names()
+    assert all(model_name in config['strategy'] for model_name in model_table_names)
+
+
+def assert_config_sections(config):
+    assert all(key in config for key in ['config', 'strategy'])
+
+
+def assert_all_model_fields_in_conf(config):
+    assert models.validate_all_model_fields_in_config(config)
+
+
+class TestConfiguration(object):
     def test_create_from_models(self):
-        model_table_names = models.get_model_table_names()
         configuration_instance = Configuration.from_models()
         config = configuration_instance.config
 
-        assert all(key in config for key in ['config', 'strategy'])
-        assert all(model_name in config['strategy'] for model_name in model_table_names)
+        assert_config_sections(config)
+        assert_all_model_fields_in_conf(config)
+
+    @patch(builtins_open, new_callable=MockOpen)
+    def test_create_from_file(self, mocked_open):
+        Configuration().write_configuration_file()
+        with open(Configuration().standard_file_path, 'r') as conf_file:
+            config = Configuration.from_file(conf_file)
+            assert config.config
+
+    @patch(builtins_open, new_callable=MockOpen)
+    def test_write_empty_configuration_file(self, mocked_open):
+        Configuration().write_configuration_file()
+        with open(Configuration().standard_file_path, 'r') as conf_file:
+            conf = yaml.load(conf_file)
+            assert conf
+
+    @patch(builtins_open, new_callable=MockOpen)
+    def test_write_full_configuration_file(self, mocked_open):
+        config = Configuration.from_models()
+        config.write_configuration_file()
+        with open(Configuration().standard_file_path, 'r') as conf_file:
+            conf = yaml.load(conf_file)
+            assert_all_model_fields_in_conf(conf)
