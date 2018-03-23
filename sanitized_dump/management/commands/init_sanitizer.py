66882e3e8084e58c814708fc098f22d415659dad
Frank Wickström <frank.wickstrom@anders.fi>
2018-03-23 07:17:37 +0200

    Do not use locale info in init_sanitizer tests

--- sanitized_dump/management/commands/init_sanitizer.py
+++ sanitized_dump/management/commands/init_sanitizer.py
@@ -6,6 +6,17 @@ from sanitized_dump.config import Configuration
 class Command(BaseCommand):
     help = 'Create a sanitizer configuration'
 
+    # Since we are not creating database content with the command this should
+    # be a safe thing to do.
+    # The reason for disabling locale is to be able to use mock_open. If it is
+    # enabled then tests will start failing due to open() being mocked and not
+    # returning a proper file object which is needed in some version of django
+    # when it runs a command.
+    #
+    # https://docs.djangoproject.com/en/2.0/howto/custom-management-commands
+    # /django.core.management.BaseCommand.leave_locale_alone
+    leave_locale_alone = True
+
     def handle(self, *args, **options):
         self.stdout.write('Creating initial configuration...')
         conf = Configuration.from_models()
