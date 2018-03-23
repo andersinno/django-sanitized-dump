7793b16f65e8335aae6945af31789de3eebe5ab5
Frank Wickström <frank.wickstrom@anders.fi>
2018-03-23 10:24:02 +0200

    Rename sanitized_dump command to create_sanitized_dump

--- sanitized_dump/management/commands/create_sanitized_dump.py
+++ sanitized_dump/management/commands/create_sanitized_dump.py
@@ -5,4 +5,6 @@ class Command(BaseCommand):
     help = 'Create sanitized database dump'
 
     def handle(self, *args, **options):
+        # TODO: Create a database dump using the current django settings and then sanitize the data
+        # and warn about diffs between the current model state and the sanitizer configuration.
         self.stdout.write('Creating sanitized dump...')
