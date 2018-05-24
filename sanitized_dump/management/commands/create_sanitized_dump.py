acf4866f8b20fc2338ce3b613f1c5b1a607f768b
Tuomas Suutari <tuomas.suutari@anders.fi>
2018-05-24 12:25:03 +0300

    create_sanitized_dump: No status messages to stdout
    Make the "Creating sanitized dump..." message go to stderr instead of
    stdout, because otherwise it will mess up the actual dump contents
    written to stdout.  Also add verbosity flag so that it is possible to
    disable that message totally by passing `-v0` to the command.

--- sanitized_dump/management/commands/create_sanitized_dump.py
+++ sanitized_dump/management/commands/create_sanitized_dump.py
@@ -8,8 +8,9 @@ from sanitized_dump.utils.db import db_setting_to_db_string
 class Command(DBReadonlyCommand):
     help = 'Create sanitized database dump'
 
-    def handle(self, *args, **options):
-        self.stdout.write('Creating sanitized dump...')
+    def handle(self, verbosity=1, *args, **options):
+        if verbosity >= 1:
+            self.stderr.write('Creating sanitized dump...')
 
         database_string = db_setting_to_db_string(settings.DATABASES)
         call(["database-sanitizer", '-c', '.sanitizerconfig', database_string])
